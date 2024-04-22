2a379620360a7b62ecded966d9488dfe9b693be2
1201132 <1201132@isep.ipp.pt>
2024-04-22 17:53:47 +0100

    clean code

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import Navigation from './app/Navigation';
-
+import React, { useEffect, useState } from "react";
+import Navigation from "./app/Navigation";
 
 export default function App() {
-  return (
-    <Navigation>
-    </Navigation>
-  );
-};
+  return <Navigation></Navigation>;
+}
--- app/Navigation.js
+++ app/Navigation.js
@@ -1,62 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { onAuthStateChanged } from 'firebase/auth';
-import Login from './screens/Login';
-import Home from './screens/Home';
-import Details from './screens/Details';
-import Signup from './screens/Signup';
-import { FIREBASE_AUTH } from '../Firebase.config';
-
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+import Signup from "./screens/Signup";
+import { FIREBASE_AUTH } from "../Firebase.config";
 
 const Stack = createNativeStackNavigator();
 
 const InsideStack = createNativeStackNavigator();
 const LoginStack = createNativeStackNavigator();
 
-function InsideLayout(){
-  return(
+function InsideLayout() {
+  return (
     <InsideStack.Navigator>
-      <InsideStack.Screen name='Home' component={Home}/>
-      <InsideStack.Screen name='Details' component={Details}/>
+      <InsideStack.Screen name="Home" component={Home} />
+      <InsideStack.Screen name="Details" component={Details} />
     </InsideStack.Navigator>
-  )
+  );
 }
 
-function LoginLayout(){
-  return(
+function LoginLayout() {
+  return (
     <LoginStack.Navigator>
-      <LoginStack.Screen name='Login' component={Login}/>
-      <LoginStack.Screen name='Signup' component={Signup}/>
+      <LoginStack.Screen name="Login" component={Login} />
+      <LoginStack.Screen name="Signup" component={Signup} />
     </LoginStack.Navigator>
-  )
+  );
 }
 
 const Navigation = () => {
+  const [user, setUser] = useState(null);
 
-const [user, setUser] = useState(null);
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log("user", user);
+      setUser(user);
+    });
+  }, []);
 
-
-useEffect(() => {
-onAuthStateChanged(FIREBASE_AUTH, (user)=>{
-    console.log('user', user);
-    setUser(user);
-});
-}, []);
-
-
-return (
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName="Login">
         {user ? (
-          <Stack.Screen name='Inside' component={InsideLayout} options={{headerShown: false}}/>
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          />
         ) : (
-          <Stack.Screen name='LoginHome' component={LoginLayout} options={{headerShown: false}}/>
+          <Stack.Screen
+            name="LoginHome"
+            component={LoginLayout}
+            options={{ headerShown: false }}
+          />
         )}
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
