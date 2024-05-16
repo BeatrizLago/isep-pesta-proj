fb6b9049864ac4d679860aa53840bb4fbd8b9c7d
1201132 <1201132@isep.ipp.pt>
2024-05-16 11:56:49 +0100

    added link rederrect to details page

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
 import Styles from "../Components/Styles";
 
 const Details = ({ route }) => {
@@ -7,28 +14,51 @@ const Details = ({ route }) => {
 
   return (
     <ScrollView style={Styles.detailsContainer}>
-      <Image source={{ uri: place.imageURL }} style={Styles.detailsImage}/>
+      <Image source={{ uri: place.imageURL }} style={Styles.detailsImage} />
       <View style={Styles.detailsContent}>
         <Text style={Styles.detailsTitle}>{place.name}</Text>
         <Text style={Styles.detailsCategory}>{place.category}</Text>
         <Text style={Styles.address}>
-          Endereço: {place.address ? `${place.address.street}, ${place.address.city}` : "Endereço não disponível"}
+          Endereço:{" "}
+          {place.address
+            ? `${place.address.street}, ${place.address.city}`
+            : "Endereço não disponível"}
         </Text>
-        <Text style={Styles.mbottom}>Telefone: {place.phoneNumber || "Telefone não disponível"}</Text>
-        <Text style={Styles.mbottom}>Email: {place.email || "Email não disponível"}</Text>
+        <Text style={Styles.mbottom}>
+          Telefone: {place.phoneNumber || "Telefone não disponível"}
+        </Text>
+        <Text style={Styles.mbottom}>
+          Email: {place.email || "Email não disponível"}
+        </Text>
         <Text style={Styles.detailsSubtitle}>Acessibilidade:</Text>
         <Text>
-          Estacionamento Prioritário: {place.accessibility?.parking ? "Disponível" : "Não Disponível"}
-          {"\n"}Entrada: {place.accessibility?.entrance ? "Acessível" : "Não Acessível"}
-          {"\n"}Casa de Banho de Deficientes: {place.accessibility?.handicapBathroom ? "Disponível" : "Não Disponível"}
-          {"\n"}Circulação Interna: {place.accessibility?.internalCirculation ? "Acessível" : "Não Acessível"}
+          Estacionamento Prioritário:{" "}
+          {place.accessibility?.parking ? "Disponível" : "Não Disponível"}
+          {"\n"}Entrada:{" "}
+          {place.accessibility?.entrance ? "Acessível" : "Não Acessível"}
+          {"\n"}Casa de Banho de Deficientes:{" "}
+          {place.accessibility?.handicapBathroom
+            ? "Disponível"
+            : "Não Disponível"}
+          {"\n"}Circulação Interna:{" "}
+          {place.accessibility?.internalCirculation
+            ? "Acessível"
+            : "Não Acessível"}
         </Text>
-        <Text style={Styles.detailsSubtitle2}>Dimensões para Cadeiras de Rodas:</Text>
+        <Text style={Styles.detailsSubtitle2}>
+          Dimensões para Cadeiras de Rodas:
+        </Text>
         <Text style={Styles.mbottom}>
-          Largura: {place.wheelchair ? place.wheelchair.width : "Não Especificado"}
-          {"\n"}Altura: {place.wheelchair ? place.wheelchair.height : "Não Especificado"}
+          Largura:{" "}
+          {place.wheelchair ? place.wheelchair.width : "Não Especificado"}
+          {"\n"}Altura:{" "}
+          {place.wheelchair ? place.wheelchair.height : "Não Especificado"}
         </Text>
-        <Text style={Styles.detailsSiteURL}>Website: {place.siteURL || "Nao disponível"}</Text>
+        <TouchableOpacity onPress={() => Linking.openURL(place.siteURL)}>
+          <Text style={Styles.detailsSiteURL}>
+            Website: {place.siteURL || "Nao disponível"}
+          </Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
