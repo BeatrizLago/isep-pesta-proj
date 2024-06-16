a753938342fdfb5a238119f3e8408378013810f5
1201132 <1201132@isep.ipp.pt>
2024-06-16 19:08:08 +0100

    map screen fix

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import {
-  View,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
+import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
 import MapComponent from "../../components/map/MapComponent";
 import SearchBar from "../../components/searchbar/SearchBar";
 import { Styles } from "./Home.styles";
@@ -11,12 +7,15 @@ import ActivityLoader from "../../components/activityloader/ActivityLoader";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLocations } from "../../state/actions/locationAction";
 import MyFilterButtons from "../../components/myfilterbuttons/MyFilterButtons";
+import MyFilter from "../../components/myfilter/MyFilter";
 
 const Home = ({ navigation }) => {
   const dispatch = useDispatch();
   const locations = useSelector((state) => state.location.locations);
+  const user = useSelector((state) => state.user.userInfo);
   const [loading, setLoading] = useState(true);
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
   const [showMap, setShowMap] = useState(true);
   const [showFilter, setShowFilter] = useState(false);
 
@@ -55,32 +54,47 @@ const Home = ({ navigation }) => {
     setShowMap((prevShowMap) => !prevShowMap);
   }, []);
 
+  const toggleFilter = useCallback(() => {
+    setShowFilter((prev) => !prev);
+  }, []);
+
   const clearFilters = useCallback(() => {
+    setSelectedFilters([]);
     setFilteredData(locations); // Reset filtered data to original data
   }, [locations]);
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={Styles.container}>
-        <MyFilterButtons
-          toggleFilter={() => setShowFilter((prev) => !prev)}
-          clearFilters={clearFilters}
-          showFilter={showFilter}
-        />
-        <SearchBar handleSearch={handleSearch} />
-        <View style={Styles.mapContainerScreen}>
-          {loading ? (
-            <ActivityLoader />
-          ) : (
-            showMap && (
-              <MapComponent
-                destination={null}
-                portugalCenter={portugalCenter}
-                locations={filteredData}
-              />
-            )
-          )}
-        </View>
+        {loading ? (
+          <ActivityLoader />
+        ) : (
+          <>
+            <MyFilterButtons
+              toggleFilter={toggleFilter}
+              clearFilters={clearFilters}
+              showFilter={showFilter}
+            />
+            <MyFilter
+              showFilter={showFilter}
+              data={locations}
+              selectedFilters={selectedFilters}
+              setFilteredData={setFilteredData}
+              onFilterChange={setSelectedFilters}
+              user={user}
+            />
+            <View style={Styles.mapContainerScreen}>
+              {showMap && (
+                <MapComponent
+                  destination={null}
+                  portugalCenter={portugalCenter}
+                  locations={filteredData}
+                />
+              )}
+              {showMap && <SearchBar handleSearch={handleSearch} />}
+            </View>
+          </>
+        )}
       </View>
     </TouchableWithoutFeedback>
   );
